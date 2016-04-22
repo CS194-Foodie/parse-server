17de994bcc059211022a2c682ea1c788bcce4043
Nick Troccoli <nicktroccoli28@gmail.com>
2016-04-22 01:51:39 -0700

    Testing Parse logging

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 var yelp = require('./yelpHelpers.js');
+console.log("Hello world!");
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
