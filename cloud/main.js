3683787cd9e45c9b3fb42e718428fe3817693b8d
Nick Troccoli <nicktroccoli28@gmail.com>
2016-05-11 21:46:44 -0700

    Testing

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,15 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+
+// Send a restaurant name, we will return all matching restaurants
+Parse.Cloud.define('eventQuery', function(req, res) {
+	var Event = Parse.Object.extend("Event"); // specify name of type you're querying for
+	var query = new Parse.Query(Event); // makes a new query over Events
+	var restuarantName = req.params.restuarantName;
+	query.equalTo("restaurantName", restuarantName);
+	query.first().then(function(response) {
+		res.success(response);
+	});
+});
