73fa7be28bc41456a85af80cc604b631d1dfe206
Nick Troccoli <nicktroccoli28@gmail.com>
2016-05-27 03:55:12 -0700

    Changed getUserStatus from session to userID lookup
    Weird bug that only popped up on Heroku, not locally

--- cloud/userStatus.js
+++ cloud/userStatus.js
@@ -1,7 +1,7 @@
 /* CLOUD FUNCTION: getUserStatus
  * --------------------------------
  * Request params:
- * 		sessionToken - the session token for the user to return the status for
+ * 		userId - the user ID for the user to return the status for
  *
  * Response format:
  *		"status" field - either "FREE", "ATTENDING", "ORGANIZING", or "WAITING"
@@ -14,9 +14,14 @@
  * containing info about this user.
  */
 Parse.Cloud.define("getUserStatus", function(req, res) {
-	
-	// Sign in as the given user
-	Parse.User.become(req.params.sessionToken).then(function(user) {
+	Parse.Cloud.useMasterKey();
+
+	// Get the given user
+	var user = null;
+	var query = new Parse.Query(Parse.User);
+	query.equalTo("objectId", req.params.userId);
+	query.first().then(function(foundUser) {
+		user = foundUser;
 
 		// First query for events created by this user
 		var ownEventQuery = new Parse.Query(Parse.Object.extend("Event"));
@@ -45,7 +50,7 @@ Parse.Cloud.define("getUserStatus", function(req, res) {
 			// Check if this user is attending any events
 			var attendingEventQuery = new Parse.Query(Parse.Object.extend("Event"));
 			attendingEventQuery.equalTo("isFinalized", true);
-			attendingEventQuery.equalTo("goingUsers", Parse.User.current());
+			attendingEventQuery.equalTo("goingUsers", user);
 			return attendingEventQuery.first();
 		}
 
